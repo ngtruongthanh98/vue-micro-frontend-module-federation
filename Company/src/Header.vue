73d427e9f6b087fa7c4b0f5bb931dc410cf35763
ThanhNT26 <ngtruongthanh98@gmail.com>
2022-05-30 14:14:23 +0700

    Initial commit

---
+++ Company/src/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      <header>
+          <h2> App Header</h2>
+          <p>items: {{cartCount}}</p>
+      </header>
+  </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        cartCount() {
+            return this.$store.state.cartItems
+        }
+    },
+}
+</script>
+
+<style>
+div {
+    text-align: center;
+}
+header {
+    width: full;
+    display: flex;
+    height: 5em;
+    background-color: #5CDB95;
+    color: #05386B;
+    font-size: 2em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+p {
+    margin-left: 4em;
+}
+</style>
